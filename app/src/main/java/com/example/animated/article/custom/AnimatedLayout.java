5f70605352dfb9002fe41ee90663ee5d4f3228c8
iskul <yuriyskul@gmail.com>
2020-08-14 16:13:51 +0300

    remove unused files

--- app/src/main/java/com/example/animated/article/custom/AnimatedLayout.java
+++ app/src/main/java/com/example/animated/article/custom/AnimatedLayout.java
@@ -1,11 +1,11 @@
-package com.example.animated.article.medium;
+package com.example.animated.article.custom;
 
 import android.content.Context;
+import android.graphics.Bitmap;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Path;
 import android.util.AttributeSet;
-import android.util.Log;
 import android.view.MotionEvent;
 import android.view.View;
 import android.widget.ImageView;
@@ -17,110 +17,50 @@ import com.example.animated.R;
 
 import de.hdodenhof.circleimageview.CircleImageView;
 
-public class AnimatedLayoutFixed extends ConstraintLayout {
+public class AnimatedLayout extends ConstraintLayout {
 
-    // idle layout state
     private static final int SUMMER_STATE = 1;
     private static final int WINTER_STATE = 2;
 
-    // animation state constants
     private static final int FROM_LEFT_TO_RIGHT = 1;
     private static final int FROM_RIGHT_TO_LEFT = 2;
     private static int IDLE_ANIMATION_STATE = 3;
 
-    private int mCurrentAnimation = IDLE_ANIMATION_STATE;
-    //current layout state
-    private int mIdleState = SUMMER_STATE;
-
-    // child views
     private ImageView mBackgroundImg;
     private TextView mTitleTV;
     private CircleImageView mProfileCircleIV;
 
-
-    //main parameter which defines the ui state of the views during transformation
-    // and the width of the Path for clipping
     private float mOffsetValue = -1;
+    private int mIdleState = WINTER_STATE;
+    private int mCurrentAnimation = IDLE_ANIMATION_STATE;
+    private float mPreviousTouchX = -1;
 
-    //path for clipping. Is used to make only specified area to be drawn and visible
     private Path mPath;
-    private float mPreviousTouchX;
 
-    public AnimatedLayoutFixed(Context context) {
+    private Bitmap mScreenShotBitmap;
+
+    public AnimatedLayout(Context context) {
         super(context);
         init();
     }
 
-    public AnimatedLayoutFixed(Context context, AttributeSet attrs) {
+    public AnimatedLayout(Context context, AttributeSet attrs) {
         super(context, attrs);
         init();
     }
 
-    public AnimatedLayoutFixed(Context context, AttributeSet attrs, int defStyleAttr) {
+    public AnimatedLayout(Context context, AttributeSet attrs, int defStyleAttr) {
         super(context, attrs, defStyleAttr);
         init();
-    }
-
-    private void init() {
-        mPath = new Path();
-    }
-
-    @Override
-    protected boolean drawChild(Canvas canvas, View child, long drawingTime) {
-//        if (canvas instanceof AnimatedLayoutTest.ScreenShotCanvas) {
-////            if (isChildExcluded(child)) {
-////                return false;
-////            }
-//            return super.drawChild(canvas, child, drawingTime);
-//        }
-
-        if (mCurrentAnimation != IDLE_ANIMATION_STATE) {
-//            if (isChildExcluded(child)) {
-//                return super.drawChild(canvas, child, drawingTime);
-//            }
-            Log.e("WTF", "clipping");
-            canvas.save();
-            clipToRectangle(canvas, child);
-            boolean result = super.drawChild(canvas, child, drawingTime);
-            canvas.restore();
-            return result;
-
-        } else {
-            return super.drawChild(canvas, child, drawingTime);
-        }
-    }
-
-    private void clipToRectangle(Canvas canvas, View child) {
-        mPath.reset();
-//        Log.e("clipP", "clipToRectangle mAnim= "+mCurrentAnimation);
-        if (mCurrentAnimation == FROM_LEFT_TO_RIGHT) {
-            mPath.addRect(
-                    0,
-                    0,
-                    mOffsetValue,
-                    getHeight(),
-                    Path.Direction.CW);
-            Log.e("clipP", mPath.toString());
-            canvas.clipPath(mPath);
-        } else if (mCurrentAnimation == FROM_RIGHT_TO_LEFT) {
-            mPath.addRect(
-                    mOffsetValue,
-                    0,
-                    getWidth(),
-                    getHeight(),
-                    Path.Direction.CW);
-            Log.e("clipP", mPath.toString());
-            canvas.clipPath(mPath);
-        }
     }
 
     @Override
     protected void onSizeChanged(int w, int h, int oldw, int oldh) {
         super.onSizeChanged(w, h, oldw, oldh);
         initIdleState(w);
+        mScreenShotBitmap = Bitmap.createBitmap(w, h, Bitmap.Config.ARGB_8888);
     }
 
-    //We can't use findViewById() from init() method because children are null yet
     @Override
     protected void onFinishInflate() {
         super.onFinishInflate();
@@ -129,96 +69,10 @@ public class AnimatedLayoutFixed extends ConstraintLayout {
         mProfileCircleIV = findViewById(R.id.profile_image);
     }
 
-
-    public boolean onTouchEvent(MotionEvent event) {
-
-        switch (event.getActionMasked()) {
-            case MotionEvent.ACTION_DOWN:
-                return true;
-            case MotionEvent.ACTION_MOVE:
-
-                float dx = event.getX() - mPreviousTouchX;
-                mPreviousTouchX = event.getX();
-
-                // check whether it was scrolled
-                if (dx == 0) {
-                    return false;
-                }
-
-                //check whether scrolling is  possible,
-                // if offset is 0 - scroll from left to right is impossible
-                // same when offset = fetWidth() scrolling to the right is impossible
-                if (!isScrollingPossible(dx)) {
-                    return false;
-                }
-
-                //safe scrolling if delta scroll is bigger then possible scroll distance  and clamp to the possible distance
-                float newOffset;
-                if (dx > 0) {
-                    newOffset = (mOffsetValue + dx) > getWidth() ? getWidth() : mOffsetValue + dx;
-                } else {
-                    //scroll to the left
-                    newOffset = (mOffsetValue + dx) < 0 ? 0 : mOffsetValue + dx;
-                }
-
-                // detect the moment when user actually starts to scroll
-                // and do some preparation
-                if (mCurrentAnimation == IDLE_ANIMATION_STATE) {
-                    // we are about to start scrolling - need do some preparation action
-                    mCurrentAnimation = (dx > 0)
-                            ? FROM_LEFT_TO_RIGHT : FROM_RIGHT_TO_LEFT;
-                    Log.e("ANIM", " animation = " + mCurrentAnimation);
-//                    ormScreenShot(perf);
-//                    offsetValue should be 0 or getWidth in this case
-                    if (mOffsetValue == getWidth()) {
-                        applyWinter();
-                    } else {
-                        applySummer();
-                    }
-                }
-                updateOffset(newOffset);
-                invalidate();
-                return true;
-            case MotionEvent.ACTION_CANCEL:
-            case MotionEvent.ACTION_UP:
-
-                mCurrentAnimation = IDLE_ANIMATION_STATE;
-                mPreviousTouchX = -1;
-                if (mOffsetValue < getWidth() / 2f) {
-                    mIdleState = WINTER_STATE;
-                    updateOffset(0);
-                    applyWinter();
-                } else {
-                    mIdleState = SUMMER_STATE;
-                    updateOffset(getWidth());
-                    applySummer();
-                }
-                invalidate();
-                return true;
-            default:
-                throw new IllegalStateException("Unexpected value: " + event.getActionMasked());
-        }
+    private void init() {
+        mPath = new Path();
     }
 
-    private boolean isScrollingPossible(float dx) {
-        if (dx == 0) {
-            //no scrolling
-            throw new IllegalArgumentException("dx cant not be 0 in this block");
-        }
-        return (!(dx > 0) || canScrollToRight(dx))
-                && (!(dx < 0) || canScrollToLeft(dx));
-    }
-
-    private boolean canScrollToRight(float dx) {
-        return mOffsetValue < getWidth();
-    }
-
-    private boolean canScrollToLeft(float dx) {
-        return mOffsetValue > 0;
-    }
-
-    // depending on idle state - init offsetValue variable with 0
-    // or layout width value
     private void initIdleState(int w) {
         float initOffset = (mIdleState == SUMMER_STATE) ? w : 0;
         updateOffset(initOffset);
@@ -235,6 +89,144 @@ public class AnimatedLayoutFixed extends ConstraintLayout {
             return;
         }
         mOffsetValue = newOffsetValue;
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        switch (event.getActionMasked()) {
+            case MotionEvent.ACTION_DOWN:
+                mPreviousTouchX = event.getX();
+                return true;
+            case MotionEvent.ACTION_MOVE:
+
+                float dx = event.getX() - mPreviousTouchX;
+                mPreviousTouchX = event.getX();
+
+                if (dx == 0) {
+                    return false;
+                }
+                if (!isScrollingPossible(dx)) {
+                    return false;
+                }
+
+                float newOffset;
+                if (dx > 0) {
+                    newOffset = (mOffsetValue + dx) > getWidth() ?
+                            getWidth() : mOffsetValue + dx;
+                } else {
+                    newOffset = (mOffsetValue + dx) < 0 ? 0 : mOffsetValue + dx;
+                }
+
+                if (mCurrentAnimation == IDLE_ANIMATION_STATE) {
+                    performScreenShot();
+                    if (mOffsetValue == getWidth()) {
+                        applyWinter();
+                    } else if (mOffsetValue == 0) {
+                        applySummer();
+                    } else {
+                        throw new IllegalStateException("Offset value has " +
+                                "to be only 0 or equals layout width during" +
+                                " IDLE_ANIMATION_STATE");
+                    }
+                    mCurrentAnimation = (dx > 0) ? FROM_LEFT_TO_RIGHT : FROM_RIGHT_TO_LEFT;
+                }
+                updateOffset(newOffset);
+                invalidate();
+                return true;
+            case MotionEvent.ACTION_CANCEL:
+            case MotionEvent.ACTION_UP:
+                mCurrentAnimation = IDLE_ANIMATION_STATE;
+                mPreviousTouchX = -1;
+                if (mOffsetValue < getWidth() / 2f) {
+                    mIdleState = WINTER_STATE;
+                    updateOffset(0);
+                    applyWinter();
+                } else {
+                    mIdleState = SUMMER_STATE;
+                    updateOffset(getWidth());
+                    applySummer();
+                }
+                invalidate();
+                return true;
+            default:
+                return false;
+        }
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        setWillNotDraw(false);
+        if (canvas instanceof ScreenShotCanvas) {
+            super.onDraw(canvas);
+            return;
+        }
+        if (mCurrentAnimation != IDLE_ANIMATION_STATE) {
+            displayScreenShot(canvas);
+        }
+    }
+
+    @Override
+    protected boolean drawChild(Canvas canvas, View child, long drawingTime) {
+        if (canvas instanceof ScreenShotCanvas) {
+            return super.drawChild(canvas, child, drawingTime);
+        }
+        if (mCurrentAnimation != IDLE_ANIMATION_STATE) {
+            canvas.save();
+            clipToRectangle(canvas, child);
+            boolean result = super.drawChild(canvas, child, drawingTime);
+            canvas.restore();
+            return result;
+
+        } else {
+            return super.drawChild(canvas, child, drawingTime);
+        }
+    }
+
+    private void performScreenShot() {
+        Canvas canvas = new ScreenShotCanvas(mScreenShotBitmap);
+        this.draw(canvas);
+    }
+
+    private boolean isScrollingPossible(float dx) {
+        if (dx == 0) {
+            throw new IllegalArgumentException("dx can not be 0 in this block");
+        }
+        return (!(dx > 0) || canScrollToRight(dx))
+                && (!(dx < 0) || canScrollToLeft(dx));
+    }
+
+    private boolean canScrollToRight(float dx) {
+        return mOffsetValue < getWidth();
+    }
+
+    private boolean canScrollToLeft(float dx) {
+        return mOffsetValue > 0;
+    }
+
+    private void displayScreenShot(Canvas canvas) {
+        canvas.drawBitmap(mScreenShotBitmap, 0, 0, null);
+    }
+
+    private void clipToRectangle(Canvas canvas, View child) {
+        mPath.reset();
+        if (mCurrentAnimation == FROM_LEFT_TO_RIGHT) {
+            mPath.addRect(
+                    0,
+                    0,
+                    mOffsetValue,
+                    getHeight(),
+                    Path.Direction.CW);
+            canvas.clipPath(mPath);
+
+        } else if (mCurrentAnimation == FROM_RIGHT_TO_LEFT) {
+            mPath.addRect(
+                    mOffsetValue,
+                    0,
+                    getWidth(),
+                    getHeight(),
+                    Path.Direction.CW);
+            canvas.clipPath(mPath);
+        }
     }
 
     //region Methods to change Ui elements attributes for summer or winter style
@@ -263,7 +255,8 @@ public class AnimatedLayoutFixed extends ConstraintLayout {
         mTitleTV.setTextColor(Color.BLACK);
         mTitleTV.setText("WINTER IS COMING...");
 
-        // fix bug: text view with wrap_content layout param does not resize its width and
+        // fix bug: text view with wrap_content layout param
+        // does not resize its width and
         // last word of new text will not displayed if new string is longer
         mTitleTV.post(new Runnable() {
             @Override
@@ -296,4 +289,10 @@ public class AnimatedLayoutFixed extends ConstraintLayout {
     }
     //endregion
 
+    private static class ScreenShotCanvas extends Canvas {
+        ScreenShotCanvas(Bitmap bitmap) {
+            super(bitmap);
+        }
+    }
 }
+
