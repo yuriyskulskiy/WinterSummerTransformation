dd59ccc5b603e722c2769cfe67d454706aa5be94
iskul <yuriyskul@gmail.com>
2020-08-16 03:30:28 +0300

    implement part_2 functionality

--- app/src/main/java/com/example/animated/article/custom/AnimatedLayout.java
+++ app/src/main/java/com/example/animated/article/custom/AnimatedLayout.java
@@ -29,6 +29,7 @@ public class AnimatedLayout extends ConstraintLayout {
     private ImageView mBackgroundImg;
     private TextView mTitleTV;
     private CircleImageView mProfileCircleIV;
+    private CircleImageView mWeatherIcon;
 
     private float mOffsetValue = -1;
     private int mIdleState = WINTER_STATE;
@@ -67,6 +68,7 @@ public class AnimatedLayout extends ConstraintLayout {
         mBackgroundImg = findViewById(R.id.background_image);
         mTitleTV = findViewById(R.id.title_tv);
         mProfileCircleIV = findViewById(R.id.profile_image);
+        mWeatherIcon = findViewById(R.id.weatherIconIV);
     }
 
     private void init() {
@@ -89,6 +91,34 @@ public class AnimatedLayout extends ConstraintLayout {
             return;
         }
         mOffsetValue = newOffsetValue;
+        updateIconView();
+    }
+
+    private void updateIconView() {
+        float oldTranslatePosition = mWeatherIcon.getTranslationX();
+        float newTranslatePosition;
+        float newAlpha;
+
+        if (mOffsetValue < getWidth() / 2f) {
+            newTranslatePosition = mOffsetValue;
+            if (oldTranslatePosition >= 0) {
+                applyWinterIcon();
+            }
+            float alphaPass = getWidth() / 2f;
+            newAlpha = 1 - newTranslatePosition / alphaPass;
+
+        } else {
+            newTranslatePosition = mOffsetValue - getWidth();
+            if (oldTranslatePosition <= 0) {
+                applySummerIcon();
+            }
+
+            float alphaPass = getWidth() / 2f;
+            newAlpha = 1 + newTranslatePosition / alphaPass;
+            mWeatherIcon.setAlpha(newAlpha);
+        }
+        mWeatherIcon.setAlpha(newAlpha);
+        mWeatherIcon.setTranslationX(newTranslatePosition);
     }
 
     @Override
@@ -168,9 +198,15 @@ public class AnimatedLayout extends ConstraintLayout {
     @Override
     protected boolean drawChild(Canvas canvas, View child, long drawingTime) {
         if (canvas instanceof ScreenShotCanvas) {
+            if (isChildExcluded(child)) {
+                return false;
+            }
             return super.drawChild(canvas, child, drawingTime);
         }
         if (mCurrentAnimation != IDLE_ANIMATION_STATE) {
+            if (isChildExcluded(child)) {
+                return super.drawChild(canvas, child, drawingTime);
+            }
             canvas.save();
             clipToRectangle(canvas, child);
             boolean result = super.drawChild(canvas, child, drawingTime);
@@ -227,6 +263,10 @@ public class AnimatedLayout extends ConstraintLayout {
                     Path.Direction.CW);
             canvas.clipPath(mPath);
         }
+    }
+
+    private boolean isChildExcluded(View child) {
+        return child.getId() == R.id.weatherIconIV;
     }
 
     //region Methods to change Ui elements attributes for summer or winter style
@@ -286,6 +326,18 @@ public class AnimatedLayout extends ConstraintLayout {
     private void applyWinterProfile() {
         mProfileCircleIV.setBorderColor(Color.BLACK);
         mProfileCircleIV.setImageResource(R.drawable.night_king_200x200);
+    }
+
+    private void applySummerIcon() {
+        mWeatherIcon.setImageResource(R.drawable.ic_sun);
+        mWeatherIcon.setCircleBackgroundColorResource(R.color.weather_icon_background_summer);
+        mWeatherIcon.setBorderColor(Color.WHITE);
+    }
+
+    private void applyWinterIcon() {
+        mWeatherIcon.setCircleBackgroundColorResource(R.color.snowflake_background);
+        mWeatherIcon.setImageResource(R.drawable.ic_snow);
+        mWeatherIcon.setBorderColor(Color.BLACK);
     }
     //endregion
 
